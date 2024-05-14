67ae3118d17e8c001b514f8a6bfa0de9d947de95
iliasbased <iliasbased@gmail.com>
2024-05-14 10:13:33 +0100

    logo transition

--- src/pages/SignUpLogin.js
+++ src/pages/SignUpLogin.js
@@ -1,27 +1,47 @@
 import Button from "../components/Button"
 import StackLayout from "../layouts/StackLayout"
-
+import { useRef, useEffect,useState } from 'react';
 import { useNavigate } from "react-router-dom"
 
 function SignUpLogin() {
+	const [loaded, setLoaded] = useState(false);
+	const [showLogin, setShowLogin] = useState(false);
 	const navigate = useNavigate()
 
+    useEffect(() => {
+		setTimeout(()=>{
+			setLoaded(true)
+			setTimeout(()=>{
+				setShowLogin(true)
+			},3000)
+		}, 100)
+    }, [])
+
+	if (showLogin) {
+		return (
+			<StackLayout className={`transition-opacity ease-in duration-2000 ${showLogin ? "opacity-100" : "opacity-0"}`}>
+				<Button
+					onClick={() => navigate("/category/select")}
+				>
+					i need help ❤️
+				</Button>
+				<Button
+					onClick={() => navigate("/category/submit")}
+				>
+					i want to help 🥰
+				</Button>
+			</StackLayout>
+		)
+	} 
+	
 	return (
 		<StackLayout>
-			<Button
-				className="w-full"
-				onClick={() => navigate("/category/select")}
-			>
-				I NEED 👍
-			</Button>
-			<Button
-				className="w-full"
-				onClick={() => navigate("/category/submit")}
-			>
-				I HAVE 👎
-			</Button>
+			<div className={`logo text-2xl transition-opacity ease-in duration-2000 ${loaded ? "opacity-100" : "opacity-0"}`}>
+				SOLIDARITY
+			</div>
 		</StackLayout>
 	)
+
 }
 
 export default SignUpLogin
